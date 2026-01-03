cd92fd84cc6a6ca43511becdfb8b0c5ab30f01ac
natie1515 <frejulian57@gmail.com>
2026-01-03 00:27:39 -0400

    Update info-menu.js

--- comandos/info-menu.js
+++ comandos/info-menu.js
@@ -340,7 +340,7 @@ let handler = async (m, { conn }) => {
                   name: 'cta_url',
                   buttonParamsJson: JSON.stringify({
                     display_text: '✿ 𝗛𝗮𝘇𝘁𝗲 𝗦𝘂𝗯𝗕𝗼𝘁',
-                    url: 'yukibotnew.sytes.net:3170'
+                    url: 'http://172.96.172.213:25407/login'
                   })
                 },
                 {
