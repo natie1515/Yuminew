23af4f548c1a735f756ea328424f1843a20c31d6
natie1515 <frejulian57@gmail.com>
2026-01-03 21:29:29 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   nombrebot: 'Y͟u͟m͟ï̵̬͟͜𝐁o̸t̸',
   moneda: '$ᴅᴏʟᴀʀᴇs',
-  apikey: 'YumiBot_API_x92A', // Pon tu apikey aqui, consiguela en: https://api-adonix.ultraplus.click
+  apikey: 'AdonixKey08xp3q3055', // Pon tu apikey aqui, consiguela en: https://api-adonix.ultraplus.click
   prefijo: '.',
 
   owner: [
